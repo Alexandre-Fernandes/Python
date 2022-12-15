91a49d4b0198eb3246b868853a58b213162a8227
Alexandre Pires <alexandrefpires_17@yahoo.com.br>
2022-12-15 13:00:12 -0300

    Funçoes e Strings

--- Projeto 1/app.py
+++ Projeto 1/app.py
@@ -6,7 +6,7 @@ nome = 'Alex'
 print(nome)
 # Posso usar aspas simples ou duplas para printa#
 
-# variable
+# VARIABLES
 
 personagem_nome = 'Marcos'
 pesonagem_idade = '19'
@@ -15,3 +15,19 @@ personagem_esporte = 'Futebol'
 print('Meu nome e ' + personagem_nome + ',')
 print('eu tenho ' + pesonagem_idade + ' anos de idade.')
 print('Meu esporte favorito e o ' + personagem_esporte)
+
+# STRINGS
+
+print('Wallace \n the Lion')  # O \n coloca toda continuação na segunda linha
+# consigmos adicionar a aspas na string  usando a barra
+print('Wallace \' the Lion')
+# O +  e um operador logico usado para concatenar valores
+
+# utilizamos o . para marcar uma função neste caso to tranfomando o valor em Maiusculo
+print(personagem_esporte.upper())
+
+valorNome = 'Alexandre Fernandes Pires'
+print(valorNome[5])  # este e o vetor index ele sempre vai iniciar do 0
+print(valorNome.index('xa'))  # aqui ele aponta onde começa o xa do vertor
+print(len(valorNome))  # conta o tamanho maximo do campo
+print(valorNome.count('a' or 'A'))  # quantos A existem
