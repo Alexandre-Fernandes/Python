ae1af28e193c8a05cea145cf6a64567b9f0eb32f
Alexandre Pires <alexandrefpires_17@yahoo.com.br>
2022-12-15 12:38:14 -0300

    incrementando variaveis

--- Projeto 1/app.py
+++ Projeto 1/app.py
@@ -5,3 +5,13 @@ print('Olá Mundo!')
 nome = 'Alex'
 print(nome)
 # Posso usar aspas simples ou duplas para printa#
+
+# variable
+
+personagem_nome = 'Marcos'
+pesonagem_idade = '19'
+personagem_esporte = 'Futebol'
+# aqui e manipulação de variaveis
+print('Meu nome e ' + personagem_nome + ',')
+print('eu tenho ' + pesonagem_idade + ' anos de idade.')
+print('Meu esporte favorito e o ' + personagem_esporte)
