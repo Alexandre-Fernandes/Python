28bfaa67de58726688767ab265d640e949cfccb7
Alexandre Pires <alexandrefpires_17@yahoo.com.br>
2022-12-15 13:10:50 -0300

    Numbers

--- Projeto 1/app.py
+++ Projeto 1/app.py
@@ -33,12 +33,33 @@ print(len(valorNome))  # conta o tamanho maximo do campo
 print(valorNome.count('a' or 'A'))  # quantos A existem
 
 
-#Number
+# Number
 
-print (2)
+print(2)
 
-print (-9)
+print(-9)
 
-print (2.04500)
+print(2.04500)
+
+# soma
+print(2 + 8.2)
+
+# multiplicação
+print(2 * 8 + 3)  # para voce exigir a soma primeiro so colocar o parenteses antes
 
 
+# Variaveis de String numeros
+
+num1 = 5
+num2 = 10
+print(num1 + num2)
+# convertemos para string o numero para poder converter-los
+print(str(num1) + 'e meu numero da sorte')
+
+# funções
+
+print(min(2, 9))
+
+print(max(2, 9))
+
+print(pow(2, 9))  # Potencia 2 elevado a 9
