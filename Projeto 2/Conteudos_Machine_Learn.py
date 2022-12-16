34b4f6c5575df69c3397cdb87f15a2e81e53c36b
Alexandre Pires <alexandrefpires_17@yahoo.com.br>
2022-12-16 10:40:53 -0300

    update

---
+++ Projeto 2/Conteudos_Machine_Learn.py
@@ -0,0 +1,26 @@
+#Bibliotecas necessarias para trabalhar com Machine Learning
+#pip install scipy
+#pip install sklearn #biblioteca principal se não funcionar usa o scikit-learn
+
+#ISTALAR O JUPTER NOTEBOOK ELE VAI RODAR UM SERVIDOR LOCAL
+#pip install ipython
+#pip install ipython[notebook]
+#
+#
+# 
+# #OBS: #pode haver erro na instalação então devemos corrigir:
+# COPIAR E SALVAR TIPO .REG a informação a baixo tira o hashtag
+
+#Windows Registry Editor Version 5.00
+
+#[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem]
+#"LongPathsEnabled"=dword:00000001
+
+#O windows caminho da instalação e muito grande acaba causando o erro.
+#
+#
+#
+
+#para abrir e parar fechar o servidor local no terminal
+#ABRIR =>>> jupyter notebook
+#fechar =>> Crt
